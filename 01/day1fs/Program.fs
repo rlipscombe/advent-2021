490e76c9429802abdaaa610e8b495970b343a2d5
Roger Lipscombe <roger@differentpla.net>
2021-12-01 18:15:49 +0000

    Simplify it
    The difference between two overlapping windows is just the difference
    between the beginning of one and the end of the other:
    
    (X0 + X1 + X2) - (X1 + X2 + X3) == X0 - X3

--- 01/day1fs/Program.fs
+++ 01/day1fs/Program.fs
@@ -6,19 +6,17 @@ let main argv =
     let path = argv.[0]
     let inputs = File.ReadLines(path) |> Seq.map (int)
 
-    let folder acc [| a; b |] = if a < b then acc + 1 else acc
-
     let part1 =
-        inputs |> Seq.windowed 2 |> Seq.fold folder 0
+        inputs
+        |> Seq.windowed 2
+        |> Seq.fold (fun acc [| a; b |] -> if a < b then acc + 1 else acc) 0
 
     printfn "%A" part1
 
     let part2 =
         inputs
-        |> Seq.windowed 3
-        |> Seq.map Array.sum
-        |> Seq.windowed 2
-        |> Seq.fold folder 0
+        |> Seq.windowed 4
+        |> Seq.fold (fun acc [| a; _; _; b |] -> if a < b then acc + 1 else acc) 0
 
     printfn "%A" part2
 
