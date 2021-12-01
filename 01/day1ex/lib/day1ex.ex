c5e0606b02ad7110ac88b4f96c1f7cc55b194254
Roger Lipscombe <roger@differentpla.net>
2021-12-01 09:51:29 +0000

    Day 1, part 1

--- 01/day1ex/lib/day1ex.ex
+++ 01/day1ex/lib/day1ex.ex
@@ -1,5 +1,18 @@
 defmodule Day1ex do
-  def main(_args) do
-    IO.puts("Hello World!")
+  def main([path]) do
+    lines =
+      File.read!(path)
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(&String.to_integer/1)
+
+    result =
+      lines
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.reduce(0, fn [a, b], acc ->
+        if b > a, do: acc + 1, else: acc
+      end)
+
+    IO.puts("#{result}")
   end
 end
