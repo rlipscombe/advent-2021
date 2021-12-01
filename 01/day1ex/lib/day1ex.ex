74eeda85eb7897996efac6a4704303e03ab8a330
Roger Lipscombe <roger@differentpla.net>
2021-12-01 10:00:09 +0000

    Show both parts

--- 01/day1ex/lib/day1ex.ex
+++ 01/day1ex/lib/day1ex.ex
@@ -6,6 +6,15 @@ defmodule Day1ex do
       |> String.split("\n")
       |> Enum.map(&String.to_integer/1)
 
+    part1 =
+      lines
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.reduce(0, fn [a, b], acc ->
+        if b > a, do: acc + 1, else: acc
+      end)
+
+    IO.puts("Part 1 = #{part1}")
+
     chunks = lines |> Enum.chunk_every(3, 1, :discard)
 
     sums =
@@ -15,13 +24,13 @@ defmodule Day1ex do
       end)
       |> Enum.reverse()
 
-    result =
+    part2 =
       sums
       |> Enum.chunk_every(2, 1, :discard)
       |> Enum.reduce(0, fn [a, b], acc ->
         if b > a, do: acc + 1, else: acc
       end)
 
-    IO.puts("#{result}")
+    IO.puts("Part 2 = #{part2}")
   end
 end
