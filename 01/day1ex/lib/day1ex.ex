5731ed9b7d6711c8002c6d8c63fc4972808b6b46
Roger Lipscombe <roger@differentpla.net>
2021-12-01 09:55:14 +0000

    Day 1, part 2

--- 01/day1ex/lib/day1ex.ex
+++ 01/day1ex/lib/day1ex.ex
@@ -6,8 +6,17 @@ defmodule Day1ex do
       |> String.split("\n")
       |> Enum.map(&String.to_integer/1)
 
+    chunks = lines |> Enum.chunk_every(3, 1, :discard)
+
+    sums =
+      chunks
+      |> Enum.reduce([], fn [a, b, c], acc ->
+        [a + b + c | acc]
+      end)
+      |> Enum.reverse()
+
     result =
-      lines
+      sums
       |> Enum.chunk_every(2, 1, :discard)
       |> Enum.reduce(0, fn [a, b], acc ->
         if b > a, do: acc + 1, else: acc
